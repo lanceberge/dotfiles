bca0598c9cdb8c096000c959cf4441291adaa6eb
lanceberge <bergeron.lance6@gmail.com>
2020-07-03 00:30:26 -0400

    Added vim-plug setup to install script Removed .config/nvim/autoload/plug.vim

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+# Necessary packages
 sudo pacman -S --noconfirm xorg
 sudo pacman -S --noconfirm alacritty
 sudo pacman -S --noconfirm zsh
@@ -11,9 +12,15 @@ sudo pacman -S --noconfirm compton
 sudo pacman -S --noconfirm tmux
 sudo pacman -S --noconfirm fzf
 
-
 mv ~/archdotfiles ~/dotfiles
+
+# Symlinks
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-chsh -s $(which zsh)
+
+chsh -s $(which zsh) # Use zsh
+
+# Set up vim plug
+curl -fLo ~/dotfiles/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
