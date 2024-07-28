b0ea3c07b40c7068f693632c8bed2efe911e90d2
lanceberge <bergeron.lance6@gmail.com>
2024-07-28 14:23:55 -0400

    autocd fish, aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,3 +13,13 @@ function fish_user_key_bindings
         bind -M $mode \cf accept-autosuggestion execute
     end
 end
+
+source ~/dotfiles/.aliases
+set -q LS_AFTER_CD || set -xg LS_AFTER_CD true
+
+# show directory listing on directory change
+function __ls_after_cd__on_variable_pwd --on-variable PWD
+    if test "$LS_AFTER_CD" = true; and status --is-interactive
+        ls -GF
+    end
+end
