29483e9893806b368f18636a791af44389455d7a
lanceberge <bergeron.lance6@gmail.com>
2024-07-28 14:15:44 -0400

    accept autosuggestion and run, git prompt config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 function fish_prompt
     set -g __fish_git_prompt_showupstream auto
+    set -g __fish_git_prompt_showdirtystate true
+    set -g __fish_git_prompt_showcolorhints true
     printf '%s %s$ ' (prompt_pwd) (fish_git_prompt)
 end
 
@@ -7,6 +9,7 @@ function fish_user_key_bindings
     fish_vi_key_bindings
 
     for mode in insert default visual
-        bind -M $mode \cf accept-autosuggestion
+        bind -M $mode \ct accept-autosuggestion
+        bind -M $mode \cf accept-autosuggestion execute
     end
 end
