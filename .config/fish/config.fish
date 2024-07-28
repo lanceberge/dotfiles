7d870fd32ee9e144b256e3102fb2f37be974cb8a
lanceberge <bergeron.lance6@gmail.com>
2024-07-28 17:56:26 -0400

    add brew to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,3 +25,5 @@ function fish_prompt
 end
 
 bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+
+set -gx PATH /opt/homebrew/bin $PATH
