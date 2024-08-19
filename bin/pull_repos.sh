dd443642d38c31b9da8f800e19a388e9bdacf1db
lanceberge <bergeron.lance6@gmail.com>
2024-08-19 18:19:25 -0400

    fix

--- bin/pull_repos.sh
+++ bin/pull_repos.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-for dir in ~/{.emacs.d,org,org-roam,org-roam/public}; do
+for dir in ~/{dotfiles,.emacs.d,org,org-roam,org-roam/public}; do
     if [ -d "$dir" ]; then
         echo "Pulling $dir"
         git -C "$dir" pull
     fi
 done
 
-git -C ~/dotfiles fetch origin
-git -C ~/dotfiles merge origin/main
