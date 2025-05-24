b95f55cd7d01aa1c1f39b9c23b4c7ad693ec4009
lanceberge <bergeron.lance6@gmail.com>
2025-05-24 15:10:06 -0400

    neogit config

--- .config/nvim/lua/git.lua
+++ .config/nvim/lua/git.lua
@@ -1,5 +1,25 @@
 return {
   {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'sindrets/diffview.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      vim.keymap.set('n', '<leader>gs', ':Neogit<CR>')
+
+      local neogit = require 'neogit'
+      neogit.setup {
+        kind = 'vsplit', -- Open Neogit in a vertical split by default
+        -- Optional: Configure specific popups
+        commit_popup = {
+          kind = 'split', -- Use horizontal split for commit editor to avoid vsplit errors
+        },
+      }
+    end,
+  },
+  {
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
